4cee7b4a0df45b2bbb03e1a0b8c1d8686b2f30b1
SHabaj-dev <shabajansari843@gmail.com>
2023-08-16 11:22:44 +0530

    Removed animations from RecycelerView

--- app/src/main/java/com/teamtechnojam/technojam/AboutActivity.kt
+++ app/src/main/java/com/teamtechnojam/technojam/AboutActivity.kt
@@ -1,14 +1,61 @@
 package com.teamtechnojam.technojam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.teamtechnojam.technojam.databinding.ActivityAboutBinding
+import com.teamtechnojam.technojam.util.Constants.FACEBOOK_URL
+import com.teamtechnojam.technojam.util.Constants.GITHUB_URL
+import com.teamtechnojam.technojam.util.Constants.INSTAGRAM_URL
+import com.teamtechnojam.technojam.util.Constants.LINKED_IN
 
 class AboutActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityAboutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.ivGithub.setOnClickListener {
+            openUrlInBrowser(GITHUB_URL)
+        }
+        binding.ivFacebook.setOnClickListener {
+            openUrlInBrowser(FACEBOOK_URL)
+        }
+
+        binding.ivLinkedin.setOnClickListener {
+            openUrlInBrowser(LINKED_IN)
+        }
+
+        binding.ivInstagram.setOnClickListener {
+            openUrlInBrowser(INSTAGRAM_URL)
+        }
+    }
+
+    private fun openUrlInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        browserIntent.setPackage("com.android.chrome")
+
+        val packageManager = packageManager
+
+        val chromeInfo =
+            packageManager.resolveActivity(browserIntent, PackageManager.MATCH_DEFAULT_ONLY)
+        val isChromeInstalled = chromeInfo != null
+
+        if (isChromeInstalled) {
+            startActivity(browserIntent)
+        } else {
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/teamtechnojam/technojam/ui/task/TaskFragment.kt
+++ app/src/main/java/com/teamtechnojam/technojam/ui/task/TaskFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout.VERTICAL
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamtechnojam.technojam.adapter.YourTaskAdapter
 import com.teamtechnojam.technojam.databinding.FragmentTaskBinding
 import com.teamtechnojam.technojam.model.YourTaskModel
@@ -43,13 +43,8 @@ class TaskFragment : Fragment() {
 
         val adapter = YourTaskAdapter(requireContext(), listTask)
 
+        binding.rvYourTask.layoutManager = LinearLayoutManager(requireContext())
         binding.rvYourTask.adapter = adapter
-
-        binding.rvYourTask.apply {
-            setAlpha(true)
-            setInfinite(false)
-            setOrientation(VERTICAL)
-        }
 
     }
 
